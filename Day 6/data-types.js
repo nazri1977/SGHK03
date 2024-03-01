1ea3814a3b18b6cf4dcd368d1badb18afb2b5003
nazri <nazri.zainal@gmail.com>
2024-03-01 21:47:46 +0800

    day 6

---
+++ Day 6/data-types.js
@@ -0,0 +1,7 @@
+console.log("Data Types");
+
+let age=25;
+console.log(age);
+
+let name="Gab"
+console.log(typeof name);
---
+++ Day 6/js-intro.js
@@ -0,0 +1,28 @@
+// function sayHello() {
+// 	console.log("Hello, world!"); //statement
+// }
+
+// sayHello(); //camelCase
+
+// // Comments
+// // single-line comments - ctrl + / or cmd + /
+// /* This
+// is 
+// a 
+// multi-line 
+// comment  - shift + alt + A */
+
+// Variables
+// let
+let number = 18; //changeable or mutable
+number = 23;
+console.log(number);
+
+// const - constant or unchangeable
+const name = "Abbi";
+console.log(name);
+
+// var
+var age = 20;
+age = 21;
+console.log(age);
